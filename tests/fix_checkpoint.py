97e62222a0d415e722663dfbdbf217fdefb2d776
Akash Mahajan <akash7190@gmail.com>
2018-05-23 00:57:48 -0700

    checkpoint modification for transfer learning

--- tests/fix_checkpoint.py
+++ tests/fix_checkpoint.py
@@ -6,19 +6,25 @@ from src.utils import Params
 from src.graph import ModelGraph
 
 parser = argparse.ArgumentParser()
-parser.add_argument('checkpoint_dir', help="Path to directory with incompatible checkpoints")
-parser.add_argument('new_scope', help="Variable scope of new variables to exclude in first restore,\
+parser.add_argument('checkpoint_dir', help="Path to directory with checkpoints to modify")
+group = parser.add_argument_group(required=True)
+group.add_argument('--restore_scope', help="Variable scope of variables to restore")
+group.add_argument('--exclude_scope', help="Variable scope of new variables to exclude in first restore,\
  that are then randomly initialized and saved")
+parser.add_argument('--restore_dir',help="Path to directory to restore from")
 args = parser.parse_args()
 
 checkpoint_path = tf.train.latest_checkpoint(args.checkpoint_dir)
+restore_path = tf.train.latest_checkpoint(args.restore_dir) if args.restore_dir else checkpoint_path
 params = Params(os.path.join(args.checkpoint_dir,'params.json'))
 gs = tf.train.get_or_create_global_step()
 g = ModelGraph(params,'train_text2mel')
 
 save_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-new_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,args.new_scope)
-restore_vars = [var for var in save_vars if var not in new_vars]
+
+restore_vars = tf.get_collection(tf.GraphKeys,GLOBAL_VARIABLES,args.restore_scope)
+exclude_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,args.exclude_scope)
+restore_vars = [var for var in restore_vars if var not in exclude_vars]
 
 restorer = tf.train.Saver(restore_vars)
 saver = tf.train.Saver(save_vars)
